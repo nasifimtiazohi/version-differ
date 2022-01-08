865beafc2c4e5349b9e77faa5918ea2151a1f6ac
Nasif Imtiaz <nasifimtiaz88@gmail.com>
2022-01-08 17:49:17 -0500

    Bump version from 0.3.3 to 0.3.4

--- version_differ/__main__.py
+++ version_differ/__main__.py
@@ -5,7 +5,7 @@ from rich import traceback
 
 
 @click.command()
-@click.version_option(version="0.3.3", message=click.style("version-differ Version: 0.3.3"))
+@click.version_option(version="0.3.4", message=click.style("version-differ Version: 0.3.4"))
 def main() -> None:
     """version-differ."""
 
